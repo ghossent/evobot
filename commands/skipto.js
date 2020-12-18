15824cdb9a687f50d939805e4f482bfe7519461a
ghossent <76193925+ghossent@users.noreply.github.com>
2020-12-18 01:27:07 -0300

    Update skipto.js

--- commands/skipto.js
+++ commands/skipto.js
@@ -3,7 +3,7 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 module.exports = {
   name: "skipto",
   aliases: ["st"],
-  description: "Skip to the selected queue number",
+  description: "Saltar al número de cola seleccionado",
   execute(message, args) {
     if (!args.length || isNaN(args[0]))
       return message
@@ -11,7 +11,7 @@ module.exports = {
         .catch(console.error);
 
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is no queue.").catch(console.error);
+    if (!queue) return message.channel.send("No hay cola.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
     if (args[0] > queue.songs.length)
       return message.reply(`The queue is only ${queue.songs.length} songs long!`).catch(console.error);
