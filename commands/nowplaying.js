5a08afad14aca0ce81c378288854e77dc69c3e48
ghossent <76193925+ghossent@users.noreply.github.com>
2020-12-18 01:31:39 -0300

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "np",
-  description: "Show now playing song",
+  description: "Mostrar ahora reproduciendo la canción",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) return message.reply("There is nothing playing.").catch(console.error);
