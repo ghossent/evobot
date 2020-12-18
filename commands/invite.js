8bf07ec1dd6a8f1b6d8abcb0e5cad51f35105d9e
ghossent <76193925+ghossent@users.noreply.github.com>
2020-12-18 01:25:25 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,12 +1,11 @@
 module.exports = {
   name: "invite",
-  description: "Send bot invite link",
+  description: "Enviar enlace de invitación al bot",
   execute(message) {
     return message.member
       .send(
         `https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=70282305&scope=bot
-    `
-      )
+   aqui tienes el link de invitacion espero que lo invites nwn `)
       .catch(console.error);
   }
 };
