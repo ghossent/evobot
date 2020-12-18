f49cc90c5ebabc6038b843f854714f681b4e1b3e
ghossent <76193925+ghossent@users.noreply.github.com>
2020-12-18 01:27:37 -0300

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -3,18 +3,18 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 module.exports = {
   name: "volume",
   aliases: ["v"],
-  description: "Change volume of currently playing music",
+  description: "Cambiar el volumen de la música que se está reproduciendo actualmente",
   execute(message, args) {
     const queue = message.client.queue.get(message.guild.id);
 
     if (!queue) return message.reply("There is nothing playing.").catch(console.error);
     if (!canModifyQueue(message.member))
-      return message.reply("You need to join a voice channel first!").catch(console.error);
+      return message.reply("¡Primero debes unirte a un canal de voz!").catch(console.error);
 
     if (!args[0]) return message.reply(`🔊 The current volume is: **${queue.volume}%**`).catch(console.error);
-    if (isNaN(args[0])) return message.reply("Please use a number to set volume.").catch(console.error);
+    if (isNaN(args[0])) return message.reply("Utilice un número para configurar el volumen.").catch(console.error);
     if (Number(args[0]) > 100 || Number(args[0]) < 0 )
-      return message.reply("Please use a number between 0 - 100.").catch(console.error);
+      return message.reply("Utilice un número entre 0 y 100.").catch(console.error);
 
     queue.volume = args[0];
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
